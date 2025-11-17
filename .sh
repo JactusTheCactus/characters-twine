cca466be7a9c55ef68064c7a216aa7021c0a5432
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2025-11-16 22:15:28 -0800

    -

---
+++ .sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+flag() {
+	for f in "$@"; do
+		[[ -e ".flags/$f" ]] || return 1
+	done
+}
+if flag local; then
+	rm -r dist
+	tsc
+	sass src/_.scss dist/_.css --no-source-map
+	perl -0777 -pe 's/^[\S\s]+\/\/ BODY\n//g' dist/_.js > dist/_.js.tmp
+	mv dist/_.js.tmp dist/_.js
+	cp ../Characters.html index.html
+fi
