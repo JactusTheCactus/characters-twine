bd99241050abf6ecf416d4bd5ad9b89d83340fac
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2025-11-21 04:27:47 -0800

    -

--- .sh
+++ .sh
@@ -7,7 +7,6 @@ flag() {
 }
 if flag local; then
 	tweego="/home/devin/Apps/tweego-2.1.1-linux-x64/tweego"
-	rm index.html
 	echo ":: StoryStylesheet [stylesheet]" > src/StoryStylesheet.tw
 	sass src/.scss --no-source-map >> src/StoryStylesheet.tw
 	$tweego src -o index.html
