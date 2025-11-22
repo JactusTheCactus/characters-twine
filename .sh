04e4cd82e96bd41579a8e77ca76b89ffb0923cd8
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2025-11-21 16:39:56 -0800

    -

--- .sh
+++ .sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
+shopt -s expand_aliases
 flag() {
 	for f in "$@"; do
 		[[ -e ".flags/$f" ]] || return 1
 	done
 }
 if flag local; then
-	sass="sass"
-	tweego="/home/devin/Apps/tweego-2.1.1-linux-x64/tweego"
+	alias tweego=~/Apps/tweego-2.1.1-linux-x64/tweego
 else
 	npm ci
-	sass="npx sass"
-	tweego="tweego"
+	alias sass="npx sass"
 fi
-echo ":: StoryStylesheet [stylesheet]" > src/StoryStylesheet.tw
-$sass src/.scss --no-source-map >> src/StoryStylesheet.tw
-$tweego src -o index.html
+{
+	echo ":: StoryStylesheet [stylesheet]"
+	sass src/.scss --no-source-map
+} > "src/StoryStylesheet.tw"
+tweego src -o index.html
