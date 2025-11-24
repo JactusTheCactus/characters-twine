4d1251855a65e26a9b3f72b70c3d004db5df22a9
JactusTheCactus <threateninglyamusing@gmail.com>
2025-11-24 01:25:23 -0800

    Update _.ts

--- src/_.ts
+++ src/_.ts
@@ -1,1 +1,29 @@
-interface Char {names:{name:Array<string>|string;pron:Array<Array<string>>|string}species:string;sex:"Male"|"Female"|"Neuter";extra:Array<string>};(State.variables as any).characters={};fetch("src/data.json").then((response)=>response.json()).then((data:{init:{ifid:string;start:string}characters:Record<string,Char>})=>{Object.entries(data.characters).forEach(([k,v]:[string,Char])=>{v.names.name=(v.names.name as Array<string>).join(" ");v.names.pron=(v.names.pron as Array<Array<string>>).map((i)=>i.join("")).join("\u{B7}");(State.variables as any).characters[k]=v});Engine.play("Main")}).catch((error)=>console.error("Error loading JSON:",error));
+interface Char {
+names:{
+name:Array<string>|string
+pron:Array<Array<string>>|string
+}
+species:string;
+sex:"Male"|"Female"|"Neuter"
+extra:Array<string>
+}
+(State.variables as any).characters={}
+fetch("src/data.json")
+.then((response)=>response.json())
+.then((data:{
+init:{
+ifid:string
+start:string
+}characters:Record<string,Char>
+})=>{
+Object.entries(data.characters)
+.forEach(([k,v]:[string,Char])=>{
+v.names.name=(v.names.name as Array<string>)
+.join(" ");
+v.names.pron=(v.names.pron as Array<Array<string>>)
+.map((i)=>i.join(""))
+.join("\u{B7}");
+(State.variables as any).characters[k]=v});
+Engine.play("Main")
+})
+.catch((error)=>console.error("Error loading JSON:",error));
