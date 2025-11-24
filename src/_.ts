77166a52a0804afcfd5f7eb71e60760426c47fdc
JactusTheCactus <threateninglyamusing@gmail.com>
2025-11-24 01:06:53 -0800

    Update _.ts

--- src/_.ts
+++ src/_.ts
@@ -20,7 +20,7 @@ fetch("src/data.json")
 		}) => {
 			Object.entries(data.characters).forEach(
 				([k, v]: [string, Char]) => {
-					v.names.name = v.names.name
+					v.names.name = (v.names.name as Array<string>)
 						.join(" ");
 					v.names.pron = v.names.pron
 						.map((i) => i.join(""))
